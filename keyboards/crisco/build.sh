aaf6b6e566707c293120c1572b45542018b7af06
Nguyen Tran <merlinvn@gmail.com>
2022-11-10 08:13:16 -0500

    add crisco

---
+++ keyboards/crisco/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+qmk compile -kb crisco -km default
+
+cp ~/qmk_firmware/crisco_default.hex /mnt/d/Projects/qmk
+
---
+++ keyboards/crisco/build_dennytom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+qmk compile -kb crisco -km dennytom
+
+cp ~/qmk_firmware/crisco_dennytom.hex /mnt/d/Projects/qmk
+
